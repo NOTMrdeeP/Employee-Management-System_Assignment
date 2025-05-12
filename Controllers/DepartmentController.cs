56955e57004a1a23c7baff44a102a6014216da1d
alyssadamons <alyssadamons3@gmail.com>
2025-05-12 16:53:44 +0200

    Create department controller (changes/test)

--- Controllers/DepartmentController.cs
+++ Controllers/DepartmentController.cs
@@ -1,12 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmployeeManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EmployeeManagementSystem.Data;
 
 namespace EmployeeManagementSystem.Controllers
 {
     public class DepartmentController : Controller
     {
+        private readonly EmployeeDbContext _context;
+        public DepartmentController(EmployeeDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+        //GET:Create Department
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: Create Department
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Departments.Add(department);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(department);
         }
 
         public IActionResult Details()
