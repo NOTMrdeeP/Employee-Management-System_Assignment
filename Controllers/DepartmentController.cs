3505af6daeb8ca0dd4a2cda856b1de4e3fc6f361
alyssadamons <alyssadamons3@gmail.com>
2025-05-12 17:04:22 +0200

    Revert "Merge pull request #3 from NOTMrdeeP/Create-departments-controller"
    This reverts commit 357d29ef39be71890c251cef0b36ffb19507618b, reversing
    changes made to 01fbee8ab31e8ff5039abc72c752ba9270a272ec.

--- Controllers/DepartmentController.cs
+++ Controllers/DepartmentController.cs
@@ -1,40 +1,12 @@
-﻿using EmployeeManagementSystem.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using EmployeeManagementSystem.Data;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManagementSystem.Controllers
 {
     public class DepartmentController : Controller
     {
-        private readonly EmployeeDbContext _context;
-        public DepartmentController(EmployeeDbContext context)
-        {
-            _context = context;
-        }
-
         public IActionResult Index()
         {
             return View();
-        }
-        //GET:Create Department
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        //POST: Create Department
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(Department department)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Departments.Add(department);
-                _context.SaveChanges();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(department);
         }
 
         public IActionResult Details()
