98883321bb24b61a67d5476f732cb943fa1067a2
NOTMrdeeP <90851917+NOTMrdeeP@users.noreply.github.com>
2025-05-17 14:58:13 +0200

    Added Default global query filter to ensure only employees where IsActive = True are retrieved

--- Data/EmployeeDbContext.cs
+++ Data/EmployeeDbContext.cs
@@ -11,6 +11,14 @@ namespace EmployeeManagementSystem.Data
 
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Department> Departments { get; set; }
-      
+
+        //Default query to filter out IsActive = false Employee records
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Global query filter
+            modelBuilder.Entity<Employee>()
+                .HasQueryFilter(m => m.IsActive == true);
+        }
+
     }//end of class EmployeeDbContext
 }//end of namespace EmployeeManagementSystem.Data
