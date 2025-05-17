2d7360dc54629cb9d0ba114442db002f47d9d6dd
NOTMrdeeP <90851917+NOTMrdeeP@users.noreply.github.com>
2025-05-17 15:43:10 +0200

    Added View All button for employees

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -33,7 +33,7 @@ namespace EmployeeManagementSystem.Controllers
                 .IgnoreQueryFilters()
                 .ToListAsync();
 
-            return View(employees);
+            return View(nameof(Index), employees);
         }
 
         //GET: Create Employee
